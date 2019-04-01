928f48a9d002ce6dec253e131d4940a3660cc802
Võ Hải <hai.softech@gmail.com>
2019-04-01 09:38:23 +0700

    remove lose

--- weather-flutter-mvvm/lib/main.dart
+++ weather-flutter-mvvm/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
     MaterialApp(
       title: "Demo",
       theme: new ThemeData(primaryColor: Color.fromRGBO(58, 66, 86, 1.0)),
-      home: SampleAppPage(title: 'Sample app'),
+      home: HomeScreen(),
       routes: <String, WidgetBuilder>{
         //'/pageTwo': (BuildContext context) => MyPageTwo(title: 'Page two'),
       },
@@ -16,65 +16,3 @@ void main() {
   );
 }
 
-class SampleAppPage extends StatefulWidget {
-  final String title;
-  SampleAppPage({Key key, this.title}) : super(key: key);
-  @override
-  _SampleAppPageState createState() => _SampleAppPageState();
-}
-
-class _SampleAppPageState extends State<SampleAppPage> {
-  List widgets = [];
-
-  @override
-  void initState() {
-    super.initState();
-    loadData();
-  }
-
-  loadData() async {
-    String dataURL = "https://jsonplaceholder.typicode.com/posts";
-    http.Response response = await http.get(dataURL);
-    setState(() {
-      widgets = json.decode(response.body);
-    });
-  }
-
-  Widget getProgressDialog() {
-    return Center(child: CircularProgressIndicator());
-  }
-
-  Widget getListView() {
-    return ListView.builder(
-      itemCount: widgets.length,
-      itemBuilder: (BuildContext context, int index) {
-        return getRow(index);
-      }
-    );
-  }
-
-  Widget getRow(int item) {
-    return Padding(
-      padding: EdgeInsets.all(10),
-      child: Text("Row ${widgets[item]["title"]}"),
-    );
-  }
-
-  getBody() {
-    if (widgets.length == 0) {
-      return getProgressDialog();
-    } else {
-      return getListView();
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Sample App"),
-      ),
-      body: getBody()
-    );
-  }
-}
